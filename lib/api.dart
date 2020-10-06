4cd7e3b87d0cb1891038b4b3ad9153bd07f36ed9
Ziggy9263 <midaz90@gmail.com>
2020-10-06 17:29:15 -0500

    Begun product section.

--- lib/api.dart
+++ lib/api.dart
@@ -28,7 +28,8 @@ Future<AuthInfo> login(String email, String password) async {
   }
 }
 
-Future<AuthInfo> register(String email, String password, String name, [String phone, String business, String address, String taxExempt]) async {
+Future<AuthInfo> register(String email, String password, String name,
+    [String phone, String business, String address, String taxExempt]) async {
   var body = new Map<String, dynamic>();
   body['email'] = email;
   body['password'] = password;
@@ -37,6 +38,21 @@ Future<AuthInfo> register(String email, String password, String name, [String ph
     'http://order.naylorsfeed.com/api/users',
     body: body,
   );
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    var data = AuthInfo.fromJSON(json.decode(response.body));
+    return data;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to Create User');
+  }
+}
+
+Future<ProductList> getProducts() async {
+  final response = await http.get('http://order.naylorsfeed.com/api/products');
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
@@ -62,6 +78,46 @@ class AuthInfo {
       token: json['token'],
       email: json['email'],
       isAdmin: json['isAdmin'],
+    );
+  }
+}
+
+class ProductDetail {
+  final String tag;
+  final String name;
+  final String description;
+  final String category;
+  final double price;
+  final List images;
+  final bool taxExempt;
+
+
+  ProductDetail({this.tag, this.name, this.description, this.category,
+    this.price, this.images, this.taxExempt});
+
+  factory ProductDetail.fromJSON(Map<String, dynamic> json) {
+    return ProductDetail(
+      tag: json['tag'],
+      name: json['name'],
+      description: json['description'],
+      category: json['category'],
+      price: json['price'],
+      images: json['images'],
+      taxExempt: json['taxExempt']
+    )
+  }
+}
+
+class ProductList {
+  final List<ProductDetail> productList;
+
+  ProductList({this.productList});
+
+  factory ProductList.fromJSON(List<Map<String, dynamic>> json) {
+    return ProductList(
+      //productList: ProductDetail(json),
+// https://codingwithjoe.com/dart-fundamentals-working-with-json-in-dart-and-flutter/
+// The above article explains how to do this, I will be focusing on UI for a bit.
     );
   }
 }
---
+++ lib/products.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class ProductsBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      
+    )
+  }
+}
