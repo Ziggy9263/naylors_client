ee60e11503a63b618df835c533f7ab03348c9e5f
Ziggy9263 <midaz90@gmail.com>
2021-02-12 14:58:48 -0600

    Added first half of image upload code. Need to make it an iterable list with edit features, and implement Firebase (second half).

--- lib/widgets/image_upload_button.dart
+++ lib/widgets/image_upload_button.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:naylors_client/widgets/widgets.dart';
 
-class ImageUploadButton extends StatelessWidget {
+class ImageUploadButton extends StatefulWidget {
   const ImageUploadButton(
       {Key key, @required this.parent, @required this.style})
       : assert(parent != null && style != null);
@@ -10,9 +13,37 @@ class ImageUploadButton extends StatelessWidget {
   final TextStyle style;
 
   @override
+  _ImageUploadButtonState createState() =>
+      _ImageUploadButtonState(parent: parent, style: style);
+}
+
+class _ImageUploadButtonState extends State<ImageUploadButton> {
+  _ImageUploadButtonState(
+      {Key key, @required this.parent, @required this.style})
+      : assert(parent != null && style != null);
+
+  final ProductEditState parent;
+  final TextStyle style;
+
+  File _imageFile;
+  final picker = ImagePicker();
+
+  Future<void> pickImage(ImageSource source) async {
+    final pickedFile = await picker.getImage(source: source);
+
+    setState(() {
+      if (pickedFile != null) _imageFile = File(pickedFile.path);
+    });
+  }
+
+  Future<void> uploadImageToFirebase(BuildContext context) async {
+    String fileName = (_imageFile.path);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 128,
+      height: _imageFile == null ? 208 : null,
       child: Column(
         children: <Widget>[
           Stack(
@@ -36,26 +67,54 @@ class ImageUploadButton extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
-              Material(
-                color: Colors.lightBlue,
-                borderRadius: BorderRadius.circular(2.0),
-                child: InkWell(
-                  onTap: () {},
-                  child: Padding(
-                    padding: EdgeInsets.all(32.0),
-                    child:
-                        Icon(Icons.camera_alt, size: 48, color: Colors.white),
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Material(
+                      color: Colors.lightBlue,
+                      borderRadius: BorderRadius.circular(2.0),
+                      child: InkWell(
+                        onTap: () {
+                          pickImage(ImageSource.camera);
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Icon(Icons.camera_alt,
+                              size: 48, color: Colors.white),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Material(
+                      color: Colors.lightBlue,
+                      borderRadius: BorderRadius.circular(2.0),
+                      child: InkWell(
+                        onTap: () {
+                          pickImage(ImageSource.gallery);
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Icon(Icons.upload_file,
+                              size: 48, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "Image uploads are currently unavailable.",
-                    style: style,
-                    textAlign: TextAlign.center,
-                  ),
+                  child: (_imageFile == null)
+                      ? Text(
+                          "No images selected.",
+                          style: style,
+                          textAlign: TextAlign.center,
+                        )
+                      : Image.file(_imageFile),
                 ),
               ),
             ],
